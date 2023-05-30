e2ec9edec21cf804b1158d9af3ce35cc5977d6cd
Alejandro Phocco <aphoccot@unsa.edu.pe>
2023-05-29 20:50:28 -0500

    Se realizo el ejercicio e con los metodos ya inplementados

--- Ajedrez/Ejercicio2e.py
+++ Ajedrez/Ejercicio2e.py
@@ -1,2 +1,9 @@
 from interpreter import draw
 from chessPictures import *
+
+base = square.join(square.negative())
+negativeBase = base.negative()
+
+figure = negativeBase.horizontalRepeat(4)
+
+draw(figure)
